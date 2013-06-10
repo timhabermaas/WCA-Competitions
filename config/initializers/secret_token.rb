7adef95b285b1bec2cd5567915d0b6a8d197de80
Tim Habermaas <tim.habermaas@gmail.com>
2013-06-10 21:51:42 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WcaCompetitions::Application.config.secret_key_base = '1c6a447eb463ed916ec66e2cec30ff7475f199c5c8a9feae9f9d1717c1787855af282c48ae42f6882e44a5cddb01439f155be5a08653d94e0c98b20ffb8107ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WcaCompetitions::Application.config.session_store :cookie_store, key: '_wca-competitions_session'
